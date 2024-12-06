8bb041f21f427382c828d485073bcb7ab11de308
Arafat-Ahmad-Sheikh <asterisk287@gmail.com>
2024-12-06 14:48:50 +0000

    strnig, mainly newString and its properties. Studying it separately bcoz it is object type and not string unlike String

---
+++ 01_basics/03_comparision.js
@@ -0,0 +1,16 @@
+const name = "arafat"
+let hobby = "drawing"
+
+console.log("I am "+ name+" and my hobby is "+ hobby);
+console.log(`I am ${name} and my hobby is ${hobby}`);
+
+const gameName = new String('Arafat')
+
+console.log(gameName);
+console.log(gameName[0]);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('p')); //-1 means not present
